b9188d99160b049272b90b563fcc9fc4044274f8
ankitzin <ankit.zin279@gmail.com>
2023-01-21 20:41:24 +0530

    Calculator work

---
+++ src/com/ankit/Calculator.java
@@ -0,0 +1,36 @@
+package com.ankit;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(true){
+            char op = in.next().trim().charAt(0);
+            if(op == '*' || op == '/' || op == '-' || op == '+' || op == '%'){
+                int num1 = in.nextInt();
+                int num2 = in.nextInt();
+
+                if(op == '*'){
+                    System.out.println(num1 * num2);
+                }
+                if(op == '+'){
+                    System.out.println(num1 + num2);
+                }
+                if(op == '-'){
+                    System.out.println(num1 - num2);
+                }
+                if(op == '/'){
+                    if(num2!=0){
+                        System.out.println(num1/num2);
+                    }
+                }
+            } else if (op == 'x' || op == 'X') {
+                System.out.println("Thanks for using caclulcator");
+                break;
+            }else{
+                System.out.println("please use proper operators");
+            }
+        }
+    }
+}
