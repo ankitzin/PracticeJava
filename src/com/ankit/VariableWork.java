410fb0fdbafdf83ff964ea70eba1506e505c9442
ankitzin <ankit.zin279@gmail.com>
2023-01-21 14:16:15 +0530

    Day4

---
+++ src/com/ankit/VariableWork.java
@@ -0,0 +1,15 @@
+package com.ankit;
+
+import java.util.Scanner;
+
+public class VariableWork {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = 10;
+        int b = 20;
+        String val = "नमस्ते";
+        int c = 10 + a + b;
+        System.out.println(c);
+        System.out.println(val);
+    }
+}
