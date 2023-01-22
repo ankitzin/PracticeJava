4c70c3d8384c32b82341909bf255684c7873c58a
ankitzin <ankit.zin279@gmail.com>
2023-01-22 13:58:54 +0530

    Armstrong number question for 3 digit number.

--- src/com/ankit_1/methods/ArmstrongNumber.java
+++ src/com/ankit_1/methods/ArmstrongNumber.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class ArmstrongNumber {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Please Enter the Number for checknig Armstrong:");
-        int val = in.nextInt();
-        boolean flag = isArmastrong(val);
-        System.out.println(flag);
+        for(int i =100;i < 1000;i++){
+            boolean flag = isArmastrong(i);
+            if(flag){
+                System.out.println(i+" ");
+            }
+        }
     }
 
     static boolean isArmastrong(int val) {
