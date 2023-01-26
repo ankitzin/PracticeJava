0485267636455474789f10cb2b2212b5958c5883
ankitzin <ankit.zin279@gmail.com>
2023-01-27 05:29:18 +0530

    Array assignement 1 Permutation of Array.

---
+++ src/com/arraysAssignment/LeetCode1920.java
@@ -0,0 +1,24 @@
+package com.arraysAssignment;
+
+import java.util.Arrays;
+
+/*
+* Given a zero-based permutation nums (0-indexed), build an array ans of the
+* same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+* A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+*
+* */
+public class LeetCode1920 {
+    public static void main(String[] args) {
+        int[] nums = {0,2,1,5,3,4};
+        System.out.println(Arrays.toString(permutationNumbers(nums)));
+    }
+
+    static int[] permutationNumbers(int[] nums){
+        int[] ans=new int[nums.length];
+        for(int i=0; i< nums.length; i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
