e8e17fe7fb25737d7904bd0f91ebaf758c5c1b58
ankitzin <ankit.zin279@gmail.com>
2023-01-27 02:31:01 +0530

    Linear search finding an min and max in 2d array

--- src/com/linearsearch/Seach2dArray.java
+++ src/com/linearsearch/Seach2dArray.java
@@ -7,11 +7,13 @@ public class Seach2dArray {
     public static void main(String[] args) {
         int[][] arr2d = {
                 {1,2,3},
-                {4,5,6},
+                {4,0,6},
                 {7,8,9}
         };
         int target = 9;
         System.out.println(search2d(arr2d,target));
+        System.out.println(findMax(arr2d));
+        System.out.println(findMin(arr2d));
 
     }
     static List<Integer> search2d(int[][] arr2d, int target){
@@ -28,4 +30,29 @@ public class Seach2dArray {
         }
         return Arrays.asList(-1,-1);
     }
+
+    static int findMax(int[][] arr2d){
+        int max = Integer.MIN_VALUE;
+        for(int[] arr: arr2d){
+            for(int val : arr){
+                if(val>max) {
+                    max = val;
+                }
+            }
+        }
+        return max;
+    }
+
+    static int findMin(int[][] arr2d){
+        int min = Integer.MAX_VALUE;
+        for(int[] arr: arr2d){
+            for(int val : arr){
+                if(val<min) {
+                    min = val;
+                }
+            }
+        }
+        return min;
+    }
+
 }
