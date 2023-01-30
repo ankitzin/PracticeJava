3db4959c5052c6897de85ce07a7a89196ef4c26f
ankitzin <ankit.zin279@gmail.com>
2023-01-31 00:59:00 +0530

    Solving Floor number, using binary search for sorted Array

---
+++ src/com/binarysearch/problems/FloorNumber.java
@@ -0,0 +1,32 @@
+package com.binarysearch.problems;
+
+public class FloorNumber {
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,9,14,16,18};
+        int target = 15;
+        System.out.println(floorNumber(arr, target));
+    }
+
+    private static int floorNumber(int[] arr, int target) {
+        try {
+            int start = 0;
+            int end = arr.length;
+            while(start<end){
+                int mid = start + (end-start)/2;
+                if(arr[mid] == target){
+                    return arr[mid];
+                }
+                if(arr[mid] < target){
+                    start = mid + 1;
+                }else{
+                    end = mid - 1;
+                }
+            }
+            return arr[end];
+        }
+        catch (Exception error){
+            System.out.println("value not available in the array");
+            return -1;
+        }
+    }
+}
