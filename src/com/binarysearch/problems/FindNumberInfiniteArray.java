b1b4fca8a748a73c9f80d1146788c7ff490da46d
ankitzin <ankit.zin279@gmail.com>
2023-02-15 08:59:29 +0530

    search number in an infinite array.

--- src/com/binarysearch/problems/FindNumberInfiniteArray.java
+++ src/com/binarysearch/problems/FindNumberInfiniteArray.java
@@ -2,14 +2,14 @@ package com.binarysearch.problems;
 
 public class FindNumberInfiniteArray {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9,10,11,14,17,19,20};
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
         int target = 7;
         System.out.println(searchNumber(arr, target));
     }
 
     static int searchNumber(int[] arr, int target) {
 
-        int[] index=  lengthOfArray(arr,target,0);
+        int[] index=  lengthOfArray(arr,target);
         int start = index[0];
         int end =index[1];
 
@@ -29,7 +29,8 @@ public class FindNumberInfiniteArray {
         return -1;
     }
 
-    static int[] lengthOfArray(int[] arr, int target,int start){
+    static int[] lengthOfArray(int[] arr, int target){
+        int start = 0;
         int[] ans = {-1, -1};
         int end = 1;
         while(arr[end] < target){
